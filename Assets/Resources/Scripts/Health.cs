dcc88a5423a839cba8392ae44ec7f9ea8149c547
Madoka <madokamiws@163.com>
2022-08-30 18:55:25 +0800

    geo动画和health脚本

---
+++ Assets/Resources/Scripts/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public Animator[] healthItem;
+    public Animator geo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Hurt()
+    {
+        healthItem[0].SetTrigger("Hurt");
+    }
+    public IEnumerator showHealthItem()
+    {
+        for (int i = 0; i < healthItem.Length; i++)
+        {
+            healthItem[i].SetTrigger("Respawn");
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield return new WaitForSeconds(0.2f);
+        geo.Play("Enter");
+    }
+    public void HideHealthItem()
+    {
+        geo.Play("Exit");
+        for (int i = 0; i < healthItem.Length; i++)
+        {
+            healthItem[i].SetTrigger("Hide");
+        }
+    }
+}
